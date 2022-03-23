2412ca3af75d352adb4f82aad166a65a8f571797
bart0709 <bart0709@stud.kea.dk>
2022-03-23 19:22:21 +0100

    refactored changeScene() to be DRY

--- src/main/java/org/boagroup/roskildedaycare/DBUtils.java
+++ src/main/java/org/boagroup/roskildedaycare/DBUtils.java
@@ -14,22 +14,15 @@ public class DBUtils {
     public static void changeScene(ActionEvent event, String fxmFile, String title, String username) {
         Parent root = null;
 
-        if (username != null) {
-            try {
-                FXMLLoader loader = new FXMLLoader(DBUtils.class.getResource(fxmFile));
-                root = loader.load();
+        try {
+            FXMLLoader loader = new FXMLLoader(DBUtils.class.getResource(fxmFile));
+            root = loader.load();
+            if (username != null) {
                 LoggedIn loggedIn = loader.getController();
                 loggedIn.setUserInformation(username);
-
-            } catch (IOException e) {
-                e.printStackTrace();
             }
-        } else {
-            try {
-                root = FXMLLoader.load(DBUtils.class.getResource(fxmFile));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setTitle(title);
